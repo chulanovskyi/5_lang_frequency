dc9caaf6840ff5c45d699ec559ff25e82d75f778
chulanovskyi <skyrocker@mail.ru>
2016-09-23 15:18:09 +0300

    top ten words ready

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+import operator
+from string import punctuation
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf8') as file:
+        return file.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    only_chars = "".join(ch for ch in text if ch not in punctuation)
+    words_dict = dict()
+    for word in only_chars.split():
+        if word in words_dict:
+            words_dict[word] += 1
+        else:
+            words_dict[word] = 1
+    sorted_words = sorted(words_dict.items(), key=operator.itemgetter(1))
+    top_ten = sorted_words[-10:][::-1]
+    for word in top_ten:
+        print(word)
+    return top_ten
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('lorem.txt')
+    get_most_frequent_words(data)
