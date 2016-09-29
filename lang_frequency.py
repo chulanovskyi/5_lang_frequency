2dd4ad3ff6a675ffe36bea8ce175e5a4d52b4f93
chulanovskyi <skyrocker@mail.ru>
2016-09-29 13:42:25 +0300

    got rid of 'magic number'

--- lang_frequency.py
+++ lang_frequency.py
@@ -14,19 +14,19 @@ def create_parser():
     return parser
 
 
-def get_most_frequent_words(text):
+def get_unique_words_count(text):
     clear_text = "".join(ch for ch in text.lower() if ch not in punctuation)
     words_counter = Counter()
     for word in clear_text.split():
         words_counter[word] += 1
-    top_ten = words_counter.most_common(10)
-    return top_ten
+    return words_counter
 
 
 if __name__ == '__main__':
     parser = create_parser()
     params = parser.parse_args()
     data = load_data(params.name)
+    words_counter = get_unique_words_count(data)
     print('Most frequent words in text: ')
-    for word in get_most_frequent_words(data):
+    for word in words_counter.most_common(10):
         print(word)
